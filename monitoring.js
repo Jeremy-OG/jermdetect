2a6d8d99bcc83eabd7cdf235f21c78b6b2983c72
Jeremy-OG <jeremyogene7@gmail.com>
2026-06-15 21:14:00 -0500

    http method,route,status code tracker

--- monitoring.js
+++ monitoring.js
@@ -7,3 +7,10 @@ const collectDefaultMetrics = client.collectDefaultMetrics
 
 // store our metrics in register to be shown to the world
 collectDefaultMetrics({register: client.register})
+
+const httpRequestDurationMicroseconds = new client.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['method','route', 'code'],
+    buckets: [0.1,0.3,0.5,0.7,1,3,5]
+});
